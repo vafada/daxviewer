f6194235a217a0a795bb6b9ebf013d70c86ebe18
Mark <vafada@gmail.com>
2025-10-18 16:56:09 -0400

    render door and event

--- src/org/vafada/daxviewer/GeoWallRecord.java
+++ src/org/vafada/daxviewer/GeoWallRecord.java
@@ -1,5 +1,5 @@
 package org.vafada.daxviewer;
 
-public record GeoWallRecord(int row, int col, int north, int south, int east, int west, byte event, int door) {
+public record GeoWallRecord(int row, int col, int north, int south, int east, int west, int event, int door) {
 
 }
--- src/org/vafada/daxviewer/MapsContainer.java
+++ src/org/vafada/daxviewer/MapsContainer.java
@@ -38,6 +38,7 @@ public class MapsContainer extends JPanel {
             g.setColor(java.awt.Color.WHITE);
             g.fillRect(0, 0, FullMapWidth, FullMapHeight);
             drawGutter(g);
+            drawRooms(map.walls(), g);
             drawGrid(g);
 
             g.dispose();
@@ -47,6 +48,100 @@ public class MapsContainer extends JPanel {
             add(Box.createVerticalStrut(20));
         }
     }
+
+    private void drawRooms(List<GeoWallRecord> walls, Graphics2D g) {
+        for (GeoWallRecord wall : walls) {
+            int x = wall.col() * RoomSize + GutterSize;
+            int y = wall.row() * RoomSize + GutterSize;
+
+            // DrawRoomBase
+            g.setColor(new Color(85, 85, 85));
+            g.fillRect(x, y, RoomSize, RoomSize);
+
+            // DrawRoomEvent
+            {
+                g.setFont(new Font("Courier New", Font.BOLD, 14));
+
+                if (wall.event() != 0) {
+                    g.setColor(new Color(34, 139, 34));
+                    g.fillRect(x, y, RoomSize, RoomSize);
+                    if ((wall.event() & 127) > 0) {
+                        g.setColor(Color.WHITE);
+                        g.drawString(String.valueOf(wall.event() & 127), x + 10, y + 15);
+                    }
+                }
+            }
+
+            // DrawRoomWalls
+            {
+                g.setColor(new Color(170, 170, 170));
+                if (wall.north() != 0) {
+                    g.fillRect(x, y, RoomSize, WallThickness);
+                }
+                if (wall.east() != 0) {
+                    g.fillRect(x + RoomSize - WallThickness, y, WallThickness, RoomSize);
+                }
+                if (wall.south() != 0) {
+                    g.fillRect(x, y + RoomSize - WallThickness, RoomSize, WallThickness);
+                }
+                if (wall.west() != 0) {
+                    g.fillRect(x, y, WallThickness, RoomSize);
+                }
+            }
+
+            // DrawRoomDoors
+            {
+                var doorX = 0;
+                var doorY = 0;
+                var doorW = 0;
+                var doorH = 0;
+
+                var northDoor = wall.door() & 3;
+                var eastDoor = ((wall.door() >> 2) & 3);
+                var southDoor = ((wall.door() >> 4) & 3);
+                var westDoor = ((wall.door() >> 6) & 3);
+
+                Color[] doorBrushes = new Color[]{null, Color.WHITE, Color.YELLOW, Color.RED};
+
+                if (northDoor > 0) {
+                    doorX = x + (WallThickness * 2);
+                    doorY = y;
+                    doorW = RoomSize - (WallThickness * 4);
+                    doorH = WallThickness * 2;
+                    g.setColor(doorBrushes[northDoor]);
+                    g.fillRect(doorX, doorY, doorW, doorH);
+                }
+                if (eastDoor > 0) {
+                    doorX = x + RoomSize - (WallThickness * 2);
+                    doorY = y + (WallThickness * 2);
+                    doorW = WallThickness * 2;
+                    doorH = RoomSize - (WallThickness * 4);
+                    g.setColor(doorBrushes[eastDoor]);
+                    g.fillRect(doorX, doorY, doorW, doorH);
+                }
+
+                if (southDoor > 0) {
+                    doorX = x + (WallThickness * 2);
+                    doorY = y + RoomSize - (WallThickness * 2);
+                    doorW = RoomSize - (WallThickness * 4);
+                    doorH = WallThickness * 2;
+                    g.setColor(doorBrushes[southDoor]);
+                    g.fillRect(doorX, doorY, doorW, doorH);
+                }
+
+                if (westDoor > 0) {
+                    doorX = x;
+                    doorY = y + (WallThickness * 2);
+                    doorW = WallThickness * 2;
+                    doorH = RoomSize - (WallThickness * 4);
+                    g.setColor(doorBrushes[westDoor]);
+                    g.fillRect(doorX, doorY, doorW, doorH);
+                }
+            }
+        }
+
+    }
+
 
     private void drawGutter(Graphics2D g) {
         g.setFont(new Font("Courier New", Font.BOLD, 14));
